60eeef954074c5f6493a31749cc8873a69e8bb79
volodg <volodg@mail.ru>
2011-10-22 17:07:44 +0300

    test commit

---
+++ echo/yaws/ebin/ex_server.erl
@@ -0,0 +1,111 @@
+%%%-------------------------------------------------------------------
+%%% File    : ex_server.erl
+%%% Author  : Vladimir Gorbenko <gorbenko.vova@gmail.com>
+%%% Description : The Exchange server.
+%%%
+%%% Created :  22 Oct 2011 by Vladimir Gorbenko <gorbenko.vova@gmail.com>
+%%%-------------------------------------------------------------------
+-module(ex_server).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0,
+        echo_test/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link() ->
+	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%--------------------------------------------------------------------
+%% Function: create_account(Name) -> ok
+%% Description: Creates a bank account for the person with name Name
+%%--------------------------------------------------------------------
+echo_test(Msg) ->
+	io:fwrite( "In: ~p~n", [ "method called" ] ),
+	%Res = gen_server:call(?SERVER, {echo, Msg}),
+	io:fwrite( "SERVER: ~p~n", [ ?SERVER ] ),
+	Res = gen_server:call( { ?SERVER, 'server@localhost:8001' }, {echo, Msg}),
+	%Res = spawn( 'server@localhost:8001', gen_server, call, [ ?SERVER, {echo, Msg} ]),
+	io:fwrite( "Out: ~p~n", [ Res ] ),
+	Res.
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([]) ->
+  {ok, dict:new()}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call({echo, Msg}, _From, State) ->
+	{reply, Msg, State};
+handle_call(_Request, _From, State) ->
+	Reply = ok,
+	{reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------

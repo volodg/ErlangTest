3da376b47663b6e7e665c0d819d7b19508ea2be3
volodg <volodg@mail.ru>
2011-10-22 23:52:11 +0300

    test commit

--- echo/yaws/ebin/bn_server.erl
+++ echo/yaws/ebin/bn_server.erl
@@ -43,8 +43,12 @@ stop() ->
 	?SRV_NAME ! stop.
 
 run_report_timer( From, Num ) ->
-	Delay = 5000,
+	Delay = 2000,
 	timer:send_after( Delay, { push_notification, From, Num } ).
+
+send_report( To, Num ) ->
+	Str = integer_to_list(Num),
+	To ! { report, Str }.
 
 loop(State) ->
 	receive
@@ -57,16 +61,16 @@ loop(State) ->
 
 		{ push_subscribe, From } ->
 			io:fwrite( "srv: push_subscribed~n" ),
-			run_report_timer( From, 5 )
-			,bn_server:loop( State );
+			run_report_timer( From, 5 ),
+			bn_server:loop( State );
 		{ push_notification, From, 0 } ->
 			io:fwrite( "srv: push_notification: ~p~n", [0] ),
-			From ! { report, 0 },
+			send_report( From, 0 ),
 			From ! finish,
 			bn_server:loop( State );
 		{ push_notification, From, Num } ->
 			io:fwrite( "srv: push_notification: ~p~n", [Num] ),
-			From ! { report, Num },
+			send_report( From, Num ),
 			run_report_timer( From, Num - 1 ),
 			bn_server:loop( State );
 

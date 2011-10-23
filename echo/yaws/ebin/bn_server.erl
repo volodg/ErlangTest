c2425d6e3dc2ad22e227d73ca4b695bbe9b05edf
volodg <volodg@mail.ru>
2011-10-23 15:58:30 +0300

    test commit

--- echo/yaws/ebin/bn_server.erl
+++ echo/yaws/ebin/bn_server.erl
@@ -1,7 +1,7 @@
 -module(bn_server).
 
 %server api
--export([start/0,stop/0,init/0,loop/1]).
+-export([start/0,stop/0,init/0,loop/2]).
 
 -include("bn_config.hrl").
 
@@ -28,13 +28,14 @@ send_report( To, Num ) ->
 init() ->
 	io:fwrite( "Init with instruments: ~p~n", [?INSTRUMENTS] ),
 	DealerPidAndExpDateByInstrument = dict:new(),
-	loop(DealerPidAndExpDateByInstrument).
+	Instruments = sets:from_list( ?INSTRUMENTS ),
+	loop( DealerPidAndExpDateByInstrument, Instruments ).
 
-loop( DealerPidAndExpDateByInstrument ) ->
+loop( DealerPidAndExpDateByInstrument, Instruments ) ->
 	receive
 		{ echo, From, Msg } ->
 			From ! { reply, Msg },
-			bn_server:loop( DealerPidAndExpDateByInstrument );
+			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments );
 		stop ->
 			io:fwrite( "Exit normally~n" ),
 			true;
@@ -43,25 +44,25 @@ loop( DealerPidAndExpDateByInstrument ) ->
 		{ get_dealer, From, { _Instrument, _Time, _Price, _Amount } } ->
 			DealerPid = spawn( bn_dealer, dealer, [] ),
 			From ! { dealer_pid, DealerPid },
-			bn_server:loop( DealerPidAndExpDateByInstrument );
+			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments );
 
 		% test push notifications
 		{ push_subscribe, From } ->
 			io:fwrite( "srv: push_subscribed~n" ),
 			run_report_timer( From, 5 ),
-			bn_server:loop( DealerPidAndExpDateByInstrument );
+			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments );
 		{ push_notification, From, 0 } ->
 			io:fwrite( "srv: push_notification: ~p~n", [0] ),
 			send_report( From, 0 ),
 			From ! finish,
-			bn_server:loop( DealerPidAndExpDateByInstrument );
+			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments );
 		{ push_notification, From, Num } ->
 			io:fwrite( "srv: push_notification: ~p~n", [Num] ),
 			send_report( From, Num ),
 			run_report_timer( From, Num - 1 ),
-			bn_server:loop( DealerPidAndExpDateByInstrument );
+			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments );
 
 		Other ->
 			io:fwrite( "Unhandled server msg~p~n", [Other] ),
-			bn_server:loop( DealerPidAndExpDateByInstrument )
+			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments )
 	end.

c86f3e80dc9bb0dc0c022a5d94bce3361c19c8ec
volodg <volodg@mail.ru>
2011-10-23 17:27:57 +0300

    Implement dealer calculation

--- echo/yaws/ebin/bn_dealer.erl
+++ echo/yaws/ebin/bn_dealer.erl
@@ -1,22 +1,57 @@
 -module(bn_dealer).
 
--export([dealer/0]).
+-export([dealer/1]).
 
-%Instrument, Time, Price, Amount
-loop() ->
+process_deal( State, From, { DealInstrument, DealTime, DealPrice, DealAmount } ) ->
+	{ _OpenTime, OpenPrice, _ClosePrice, MinPrice, MaxPrice, TotalAmount } = State,
+
+	io:fwrite( "In Instrument: ~p~n", [DealInstrument] ),
+	io:fwrite( "In Time: ~p~n", [DealTime] ),
+	io:fwrite( "In Price: ~p~n", [DealPrice] ),
+	io:fwrite( "In Amount: ~p~n", [DealAmount] ),
+	io:fwrite( "------------------------~n" ),
+
+	NewState = case TotalAmount of
+		0 ->
+			%first deal here on instrument
+			{ _OpenTime, DealPrice, DealPrice, DealPrice, DealPrice, DealAmount };
+		_Other ->
+			RecTotalAmount = TotalAmount + DealAmount,
+			RecClosePrice = DealPrice,
+			RecMinPrice = min( MinPrice, DealPrice ),
+			RecMaxPrice = max( MaxPrice, DealPrice ),
+			{ _OpenTime, OpenPrice, RecClosePrice, RecMinPrice, RecMaxPrice, RecTotalAmount }
+	end,
+	From ! { reply, "Good deal" },
+
+	{ _NewOpenTime, NewOpenPrice, NewClosePrice, NewMinPrice, NewMaxPrice, NewTotalAmount } = NewState,
+	io:fwrite( "Out NewOpenPrice: ~p~n", [NewOpenPrice] ),
+	io:fwrite( "Out NewClosePrice: ~p~n", [NewClosePrice] ),
+	io:fwrite( "Out NewMinPrice: ~p~n", [NewMinPrice] ),
+	io:fwrite( "Out NewMaxPrice: ~p~n", [NewMaxPrice] ),
+	io:fwrite( "Out NewTotalAmount: ~p~n", [NewTotalAmount] ),
+	io:fwrite( "------------------------~n" ),
+
+	NewState.
+
+%TODO validate Instrument here
+loop( State ) ->
 	receive
 		{ From, { Instrument, Time, Price, Amount } } ->
-			io:fwrite( "Instrument: ~p~n", [Instrument] ),
-			io:fwrite( "Time: ~p~n", [Time] ),
-			io:fwrite( "Price: ~p~n", [Price] ),
-			io:fwrite( "Amount: ~p~n", [Amount] ),
-			io:fwrite( "------------------------~n" ),
-			From ! { reply, "Good" };
+			loop( process_deal( State, From, { Instrument, Time, Price, Amount } ) );
 		Other ->
 			%TODO fix this
-			io:fwrite( "Unhandled msg: ~p~n", [Other] )
+			io:fwrite( "Unhandled msg in dealer: ~p~n", [Other] ),
+			loop( State )
 	end.
 
-dealer() ->
+dealer( InstrumentName ) ->
 	%init here timer, state and etc
-	loop().
+	io:fwrite( "Start Dealer For Instrument: ~p~n", [InstrumentName] ),
+	TotalAmount = 0,
+	OpenPrice = 0,
+	ClosePrice = 0,
+	MinPrice = 0,
+	MaxPrice = 0,
+	InitState = { open_time_todo, OpenPrice, ClosePrice, MinPrice, MaxPrice, TotalAmount },
+	loop( InitState ).
--- echo/yaws/ebin/bn_server.erl
+++ echo/yaws/ebin/bn_server.erl
@@ -1,7 +1,7 @@
 -module(bn_server).
 
 %server api
--export([start/0,stop/0,init/0,loop/2]).
+-export([start/0,stop/0,init/0,loop/2,restart/0]).
 
 -include("bn_config.hrl").
 
@@ -23,6 +23,10 @@ init() ->
 stop() ->
 	?SRV_NAME ! stop.
 
+restart() ->
+	stop(),
+	start().
+
 run_report_timer( From, Num ) ->
 	Delay = 2000,
 	timer:send_after( Delay, { push_notification, From, Num } ).
@@ -31,16 +35,30 @@ send_report( To, Num ) ->
 	Str = integer_to_list(Num),
 	To ! { report, Str }.
 
-%is_element(Element, Set) -> boolean()
-process_get_dealer( DealerPidAndExpDateByInstrument, Instruments, From, { Instrument, _Time, _Price, _Amount } ) ->
-	case sets:is_element( Instrument, Instruments ) of
-		true ->
-			DealerPid = spawn( bn_dealer, dealer, [] ),
-			From ! { dealer_pid, DealerPid };
-		false ->
-			From ! { error, "Invalid instrument name" }
+%validate arguments before calling this method
+process_get_dealer_for_instrument( DealerPidAndExpDateByInstrument, From, { Instrument, _Time, _Price, _Amount } ) ->
+	{ NewDealerPidAndExpDateByInstrument, InstrumentDealerPid } = case dict:find( Instrument, DealerPidAndExpDateByInstrument ) of
+		{ ok, { DealerPid, _ExpirationDate } } ->
+			{ DealerPidAndExpDateByInstrument, DealerPid };
+		error ->
+			DealerPid = spawn( bn_dealer, dealer, [Instrument] ),
+			%TODO pass exp_date_todo according to duration and current date
+			NewState = dict:store( Instrument, { DealerPid, exp_date_todo }, DealerPidAndExpDateByInstrument ),
+			{ NewState, DealerPid }
 	end,
-	bn_server:loop( DealerPidAndExpDateByInstrument, Instruments ).
+	From ! { dealer_pid, InstrumentDealerPid },
+	NewDealerPidAndExpDateByInstrument.
+
+%validate arguments here ( date also )
+process_get_dealer( DealerPidAndExpDateByInstrument, Instruments, From, { Instrument, Time, Price, Amount } ) ->
+	NewDealerPidAndExpDateByInstrument = case sets:is_element( Instrument, Instruments ) of
+		true ->
+			process_get_dealer_for_instrument( DealerPidAndExpDateByInstrument, From, { Instrument, Time, Price, Amount } );
+		false ->
+			From ! { error, "Invalid instrument name" },
+			DealerPidAndExpDateByInstrument
+	end,
+	bn_server:loop( NewDealerPidAndExpDateByInstrument, Instruments ).
 
 loop( DealerPidAndExpDateByInstrument, Instruments ) ->
 	receive

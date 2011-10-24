ba47f97ce835ce77800357b4bae59007ade9c569
volodg <volodg@mail.ru>
2011-10-24 21:32:14 +0300

    Implement dealer calculation

--- echo/yaws/ebin/bn_server.erl
+++ echo/yaws/ebin/bn_server.erl
@@ -33,13 +33,14 @@ init() ->
 
 	bn_report:start_link(),
 
-	DealerPidAndExpDateByInstrument = dict:new(),
+	State = dict:store( dealer_info_by_instrument, dict:new(), dict:new() ),
+
 	Instruments = sets:from_list( ?INSTRUMENTS ),
 
 	StartDateTime = startDatetime(),
 	{ EndDatetime, Duration } = endDateTime( StartDateTime ),
 
-	loop( DealerPidAndExpDateByInstrument, Instruments, { StartDateTime, EndDatetime, Duration } ).
+	loop( State, Instruments, { StartDateTime, EndDatetime, Duration } ).
 
 stop() ->
 	?SRV_NAME ! stop.
@@ -57,72 +58,84 @@ send_report( To, Num ) ->
 	Str = integer_to_list(Num),
 	To ! { report, Str }.
 
-run_new_dealer_for_instrument( Instrument, DatesSettings, DealerPidAndExpDateByInstrument ) ->
+run_new_dealer_for_instrument( State, Instrument, DatesSettings ) ->
 	ExpirationDatetime = datetime:nearestExpirationDatetime( DatesSettings ),
 	DealerPid = spawn( bn_dealer, dealer, [Instrument, ExpirationDatetime] ),
-	NewState = dict:store( Instrument, { DealerPid, ExpirationDatetime }, DealerPidAndExpDateByInstrument ),
+	NewState = set_dealer_info( State, Instrument, { DealerPid, ExpirationDatetime } ),
 	{ NewState, DealerPid }.
 
 %validate arguments before calling this method
-process_get_dealer_for_instrument( DealerPidAndExpDateByInstrument, DatesSettings, From, { Instrument, _Time, _Price, _Amount } ) ->
-	{ NewDealerPidAndExpDateByInstrument, InstrumentDealerPid } = case dict:find( Instrument, DealerPidAndExpDateByInstrument ) of
+process_get_dealer_for_instrument( State, DatesSettings, From, { Instrument, _Time, _Price, _Amount } ) ->
+	{ NewState, InstrumentDealerPid } = case find_dealer_info( State, Instrument ) of
 		{ ok, { DealerPid, ExpirationDate } } ->
 			NearestNextStartDate = datetime:nearestExpirationDatetime( DatesSettings ),
 			ExpirationDateExpared = datetime:datetimeEarlierThanDatetime( ExpirationDate, NearestNextStartDate ),
 		    case ExpirationDateExpared of
 				true ->
-					run_new_dealer_for_instrument( Instrument, DatesSettings, DealerPidAndExpDateByInstrument );
+					run_new_dealer_for_instrument( State, Instrument, DatesSettings );
 				false ->
-					{ DealerPidAndExpDateByInstrument, DealerPid }
+					{ State, DealerPid }
 			end;
 		error ->
-			run_new_dealer_for_instrument( Instrument, DatesSettings, DealerPidAndExpDateByInstrument )
+			run_new_dealer_for_instrument( State, Instrument, DatesSettings )
 	end,
 	From ! { dealer_pid, InstrumentDealerPid },
-	NewDealerPidAndExpDateByInstrument.
+	NewState.
 
-process_get_dealer( DealerPidAndExpDateByInstrument, Instruments, DatesSettings, From, { Instrument, Time, Price, Amount } ) ->
+process_get_dealer( State, Instruments, DatesSettings, From, { Instrument, Time, Price, Amount } ) ->
 	ValidDealArgs = bn_common:validate_deal_args( Instruments, DatesSettings, Instrument, Time, Price, Amount ),
 
-	NewDealerPidAndExpDateByInstrument = case ValidDealArgs of
+	NewState = case ValidDealArgs of
 		true ->
-			process_get_dealer_for_instrument( DealerPidAndExpDateByInstrument, DatesSettings, From, { Instrument, Time, Price, Amount } );
+			process_get_dealer_for_instrument( State, DatesSettings, From, { Instrument, Time, Price, Amount } );
 		{ error, ValidationErrorDescr } ->
 			From ! { error, ValidationErrorDescr },
-			DealerPidAndExpDateByInstrument
+			State
 	end,
-	bn_server:loop( NewDealerPidAndExpDateByInstrument, Instruments, DatesSettings ).
+	bn_server:loop( NewState, Instruments, DatesSettings ).
 
-loop( DealerPidAndExpDateByInstrument, Instruments, DatesSettings ) ->
+%returns { ok, { DealerPid, ExpirationDate } } or error
+find_dealer_info( State, Instrument ) ->
+	%io:fwrite( "Print State: ~p~n", [State] ),
+	{ ok, DealerPidAndExpDateByInstrument } = dict:find( dealer_info_by_instrument, State ),
+	dict:find( Instrument, DealerPidAndExpDateByInstrument ).
+
+%returns NewState
+set_dealer_info( State, Instrument, DealerInfo ) ->
+	{ ok, DealerPidAndExpDateByInstrument } = dict:find( dealer_info_by_instrument, State ),
+	NewDealerPidAndExpDateByInstrument = dict:store( Instrument, DealerInfo, DealerPidAndExpDateByInstrument ),
+	dict:store( dealer_info_by_instrument, NewDealerPidAndExpDateByInstrument, State ).
+
+loop( State, Instruments, DatesSettings ) ->
 	receive
 		{ echo, From, Msg } ->
 			From ! { reply, Msg },
-			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments, DatesSettings );
+			bn_server:loop( State, Instruments, DatesSettings );
 		stop ->
 			io:fwrite( "Exit normally~n" ),
 			exit(normal);
 
 		%get dealer for arguments
 		{ get_dealer, From, { Instrument, Time, Price, Amount } } ->
-			process_get_dealer( DealerPidAndExpDateByInstrument, Instruments, DatesSettings, From, { Instrument, Time, Price, Amount } );
+			process_get_dealer( State, Instruments, DatesSettings, From, { Instrument, Time, Price, Amount } );
 
 		% test push notifications
 		{ push_subscribe, From } ->
 			io:fwrite( "srv: push_subscribed~n" ),
 			run_report_timer( From, 5 ),
-			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments, DatesSettings );
+			bn_server:loop( State, Instruments, DatesSettings );
 		{ push_notification, From, 0 } ->
 			io:fwrite( "srv: push_notification: ~p~n", [0] ),
 			send_report( From, 0 ),
 			From ! finish,
-			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments, DatesSettings );
+			bn_server:loop( State, Instruments, DatesSettings );
 		{ push_notification, From, Num } ->
 			io:fwrite( "srv: push_notification: ~p~n", [Num] ),
 			send_report( From, Num ),
 			run_report_timer( From, Num - 1 ),
-			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments, DatesSettings );
+			bn_server:loop( State, Instruments, DatesSettings );
 
 		Other ->
 			io:fwrite( "Unhandled server msg~p~n", [Other] ),
-			bn_server:loop( DealerPidAndExpDateByInstrument, Instruments, DatesSettings )
+			bn_server:loop( State, Instruments, DatesSettings )
 	end.

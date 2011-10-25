627c8e19b61787f78463d97ad1e0db0847098dec
volodg <volodg@mail.ru>
2011-10-25 22:18:52 +0300

    Implement dealer calculation

--- echo/yaws/ebin/selftest.erl
+++ echo/yaws/ebin/selftest.erl
@@ -7,7 +7,6 @@
 run() ->
 	case bn_server:start() of
 		ok ->
-			io:fwrite( "test_deals~n" ),
 			case catch( test_deals() ) of
 				true ->
 					io:fwrite( "TESTS PASSED~n" );
@@ -31,7 +30,8 @@ check_dealer_response( Resp ) ->
 
 test_random_normal_deal() ->
 	Deal = bn_common:random_deal(),
-	check_dealer_response( bn_server:deal( Deal ) ).
+	{ _Instrument, DateTime, Price, Amount } = Deal,
+	check_dealer_response( bn_server:deal( { "echo3", DateTime, Price, Amount } ) ).
 
 test_invalid_instrument() ->
 	Deal = bn_common:random_deal(),
@@ -44,19 +44,53 @@ test_invalid_instrument() ->
 			throw( { error, "Server should fail this deal", Other } )
 	end.
 
+test_invalid_datetime_format() ->
+	Deal = bn_common:random_deal(),
+	{ Instrument, _DateTime, Price, Amount } = Deal,
+	InvalidDateTime = { 10, "23" },
+	NewDeal = { Instrument, InvalidDateTime, Price, Amount },
+ 	case bn_server:deal( NewDeal ) of
+		{error,_Msg} ->
+			true;
+		Other ->
+			throw( { error, "Server should fail this deal", Other } )
+	end.
+
+receive_report_loop( Instrument, OpenPrice, ClosePrice, MinPrice, MaxPrice, TotalAmount, Delay ) ->
+	receive
+		{ report, Instrument, _OpenDatetime, OpenPrice, ClosePrice, MinPrice, MaxPrice, TotalAmount } ->
+			io:fwrite( "Report received for Instrument: ~p~n", [ Instrument ] ),
+			true;
+		_Other ->
+			receive_report_loop( Instrument, OpenPrice, ClosePrice, MinPrice, MaxPrice, TotalAmount, Delay )
+		after Delay ->
+			throw( { error, "Have no valid report for instrument", [Instrument] } )
+	end.
+
 test_sum_of_deals_on_instument( Instrument ) ->
 	DateTime = { date(), time() },
-	Deal1 = { Instrument, DateTime, 1.1, 10 },
-	Deal2 = { Instrument, DateTime, 1.9, 5 },
+	Deal1 = { Instrument, DateTime, 1.4, 1 },
+	Deal2 = { Instrument, DateTime, 1.1, 2 },
+	Deal3 = { Instrument, DateTime, 1.9, 3 },
+	Deal4 = { Instrument, DateTime, 1.5, 4 },
 
 	check_dealer_response( bn_server:deal( Deal1 ) ),
 	check_dealer_response( bn_server:deal( Deal2 ) ),
+	check_dealer_response( bn_server:deal( Deal3 ) ),
+	check_dealer_response( bn_server:deal( Deal4 ) ),
 
-	true.
+	bn_report:subscribe( self() ),
+
+	NowDatetime = { date(), time() },
+	DatetimeDuration = datetime:add_second_to_datetime( ?REPORT_DURATION_SEC, NowDatetime ),
+	Delay = datetime:datetime_difference_in_seconds( NowDatetime, DatetimeDuration ) * 1000 + 10,
+	receive_report_loop( Instrument, 1.4, 1.5, 1.1, 1.9, 10, Delay ).
 
 test_deals() ->
 	test_random_normal_deal(),
 	test_invalid_instrument(),
+%	test_invalid_datetime_format(),
 
 	test_sum_of_deals_on_instument( "echo1" ),
+	test_sum_of_deals_on_instument( "echo2" ),
 	true.

2739c8dec34a9d70847ae55e65fe4653b402884d
volodg <volodg@mail.ru>
2011-10-28 00:01:42 +0300

    Implement dealer calculation

--- echo/yaws/ebin/bn_dealer.erl
+++ echo/yaws/ebin/bn_dealer.erl
@@ -22,17 +22,17 @@ process_deal( State, From, { _DealInstrument, _DealTime, DealPrice, DealAmount }
 
 	set_report_data( NewReportData, State ).
 
-send_report( DealerInstrument, State ) ->
+send_report( State ) ->
 	{ OpenTime, OpenPrice, ClosePrice, MinPrice, MaxPrice, TotalAmount } = get_report_data( State ),
 	case TotalAmount of
 		TotalAmount when TotalAmount > 0 ->
-			bn_report:notify( { report, DealerInstrument, OpenTime, OpenPrice, ClosePrice, MinPrice, MaxPrice, TotalAmount } );
+			bn_report:notify( { report, get_dealer_instrument( State ), OpenTime, OpenPrice, ClosePrice, MinPrice, MaxPrice, TotalAmount } );
 		_Other ->
 			ignore
 	end,
 	exit( normal ).
 
-loop( State, DealerInstrument ) ->
+loop( State ) ->
 	DealerDateRange = get_datetime_setting( State ),
 	{ _StartDatetime, EndDatetime, _DelaySeconds } = DealerDateRange,
 	receive
@@ -40,9 +40,9 @@ loop( State, DealerInstrument ) ->
 			Expared = not datetime:datetime_earlier_than_datetime( datetime:now_datetime(), EndDatetime ),
 			case Expared of
 				true ->
-					send_report( DealerInstrument, State );
+					send_report( State );
 				false ->
-					ValidDealArgs = bn_common:validate_deal_args( [ DealerInstrument ], DealerDateRange, Deal ),
+					ValidDealArgs = bn_common:validate_deal_args( [ get_dealer_instrument( State ) ], DealerDateRange, Deal ),
 
 					NewState = case ValidDealArgs of
 						true ->
@@ -52,13 +52,13 @@ loop( State, DealerInstrument ) ->
 							State
 					end,
 
-					loop( NewState, DealerInstrument )
+					loop( NewState )
 			end;
 		send_report ->
-			send_report( DealerInstrument, State );
+			send_report( State );
 		Other ->
 			io:fwrite( "Unhandled msg in dealer (should not happen): ~p~n", [Other] ),
-			loop( State, DealerInstrument )
+			loop( State )
 	end.
 
 dealer( InstrumentName, DatetimeSettings ) ->
@@ -79,8 +79,13 @@ dealer( InstrumentName, DatetimeSettings ) ->
 	State = set_report_data( InitialReportData, dict:new() ),
 
 	StateWithDatetime = dict:store( datetime_settings, DatetimeSettings, State ),
+	InitialState = dict:store( dealer_instrument, InstrumentName, StateWithDatetime ),
 
-	loop( StateWithDatetime, InstrumentName ).
+	loop( InitialState ).
+
+get_dealer_instrument( State ) ->
+	{ ok, DealerInstrument } = dict:find( dealer_instrument, State ),
+	DealerInstrument.
 
 get_datetime_setting( State ) ->
 	{ ok, DatetimeSetting } = dict:find( datetime_settings, State ),

dffdc11afb9bc39b5a0d1566dbc7c443f0497634
volodg <volodg@mail.ru>
2011-10-30 17:38:51 +0200

    Implement dealer calculation

--- echo/yaws/ebin/bn_config.hrl
+++ echo/yaws/ebin/bn_config.hrl
@@ -3,10 +3,13 @@
 %-define(SRV_NODE, 'server@Mac-mini-MasServer').
 
 -define(INSTRUMENTS, [ "echo1", "echo2", "echo3", "echo4", "echo5", "echo6", "echo7", "echo8", "echo9", "echo10" ]).
+%-define(INSTRUMENTS, [ "echo1", "echo2", "echo3", "echo4", "echo5" ]).
 
 -define(START_DATETIME, {{2011,1,10},{12,12,12}}). %ignored, now started from now.
 -define(REPORT_DURATION_SEC, 5).
 -define(REPORT_DURATION_NUM, 10000).
+
+-define(DEALERS_PER_INSTRUMENT, 100).
 
 -record(report, {
 		instrument,
